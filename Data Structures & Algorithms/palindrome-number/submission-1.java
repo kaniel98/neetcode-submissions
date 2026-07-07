9859971a5f4e575978f04c426deec5895777c965
Kaniel Koh <kankoh98@gmail.com>
2026-07-08 00:14:50 +0800

    Add: palindrome-number - submission-1

---
+++ Data Structures & Algorithms/palindrome-number/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int temp = x; 
+        int reversed = 0; 
+
+        while (temp != 0) {
+            int lastDigit = temp % 10;
+            reversed *= 10;
+            reversed = reversed + lastDigit; 
+            temp /= 10;
+        }
+
+        return reversed == x;
+    }
+}
